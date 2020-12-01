c7296a4c3a593164a573b7c452430ac040a47216
Terrence Chan <terrence_c@ymail.com>
2020-12-01 15:40:18 -0600

    added css

---
+++ convert.js
@@ -0,0 +1,21 @@
+const convert = (degree) => {
+  var x;
+  if (degree == "C") {
+    x = (document.getElementById("c").value * 9) / 5 + 32;
+    document.getElementById("f").value = Math.round(x);
+  } else {
+    x = ((document.getElementById("f").value - 32) * 5) / 9;
+    document.getElementById("c").value = Math.round(x);
+  }
+};
+
+const toFeet = () =>
+  (document.getElementById("feetValue").value =
+    document.getElementById("milesValue").value * 5280);
+const toMiles = () => {
+  document.getElementById("milesValue").value =
+    document.getElementById("feetValue").value / 5280;
+  //conditional ternary operator
+  var msg = document.getElementById("feetValue").value == "777" ? "LUCKY!" : "";
+  document.getElementById("msg").innerHTML = msg;
+};
