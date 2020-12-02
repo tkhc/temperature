27694cf5b1f344277694da8e7e39c57a247541e5
Terrence Chan <terrence_c@ymail.com>
2020-12-02 08:12:51 -0600

    updated converter to include kelvin

--- convert.js
+++ convert.js
@@ -1,12 +1,20 @@
 //temperature converter
-const convert = (degree) => {
-  var x;
-  if (degree == "C") {
-    x = (document.getElementById("c").value * 9) / 5 + 32;
-    document.getElementById("f").value = Math.round(x);
-  } else {
-    x = ((document.getElementById("f").value - 32) * 5) / 9;
-    document.getElementById("c").value = Math.round(x);
+const converter = (source, valNum) => {
+  valNum = parseFloat(valNum);
+  let inputFahrenheit = document.getElementById("inputFahrenheit");
+  let inputCelsius = document.getElementById("inputCelsius");
+  let inputKelvin = document.getElementById("inputKelvin");
+  if (source == "inputFahrenheit") {
+    inputCelsius.value = ((valNum - 32) / 1.8).toFixed(2);
+    inputKelvin.value = ((valNum - 32) / 1.8 + 273.15).toFixed(2);
+  }
+  if (source == "inputCelsius") {
+    inputFahrenheit.value = (valNum * 1.8 + 32).toFixed(2);
+    inputKelvin.value = (valNum + 273.15).toFixed(2);
+  }
+  if (source == "inputKelvin") {
+    inputFahrenheit.value = ((valNum - 273.15) * 1.8 + 32).toFixed(2);
+    inputCelsius.value = (valNum - 273.15).toFixed(2);
   }
 };
 
